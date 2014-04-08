430ee9c0cea533bfe2288dad64b274092ad72cf8
Tristan <tristan@rubadeau.com>
2014-04-08 01:07:43 +0000

    validate to be empty

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,24 +4,24 @@ describe Book, "validation" do
   it "is valid when it has a title attribute" do
     book = Book.new title: "Something"
     book.valid?
-    expect(book.errors[:title].empty?).to be_true
+    expect(book.errors[:title]).to be_empty
   end
   
   it "is invalid when it is missing a title attribute" do
     book = Book.new
     book.valid?
-    expect(book.errors[:title].empty?).to be_false
+    expect(book.errors[:title]).to_not be_empty
   end
   
   it "is valid when it has a pages attribute" do
     book = Book.new pages: 250, title: "Something"
     book.valid?
-    expect(book.errors[:pages].empty?).to be_true
+    expect(book.errors[:pages]).to be_empty
   end
   
   it "is invalid when it is missing a pages attribute" do
     book = Book.new
     book.valid?
-    expect(book.errors[:pages].empty?).to be_false
+    expect(book.errors[:pages]).to_not be_empty
   end
 end
