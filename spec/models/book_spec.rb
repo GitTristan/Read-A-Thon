93cca717f3bb005efbd8e39b21e3df7d73d2a6ae
Tristan <tristan@rubadeau.com>
2014-04-08 01:12:35 +0000

    Add Shoulda Gem and validate_presence_of

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,27 +1,6 @@
 require 'spec_helper'
 
 describe Book, "validation" do
-  it "is valid when it has a title attribute" do
-    book = Book.new title: "Something"
-    book.valid?
-    expect(book.errors[:title]).to be_empty
-  end
-  
-  it "is invalid when it is missing a title attribute" do
-    book = Book.new
-    book.valid?
-    expect(book.errors[:title]).to_not be_empty
-  end
-  
-  it "is valid when it has a pages attribute" do
-    book = Book.new pages: 250, title: "Something"
-    book.valid?
-    expect(book.errors[:pages]).to be_empty
-  end
-  
-  it "is invalid when it is missing a pages attribute" do
-    book = Book.new
-    book.valid?
-    expect(book.errors[:pages]).to_not be_empty
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :pages }
 end
