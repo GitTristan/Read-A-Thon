73202bcff210c8c49b62a6fdef7281a5ee2122a8
Tristan <tristan@rubadeau.com>
2014-04-08 01:05:52 +0000

    Book test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
-describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Book, "validation" do
+  it "is valid when it has a title attribute" do
+    book = Book.new title: "Something"
+    book.valid?
+    expect(book.errors[:title].empty?).to be_true
+  end
+  
+  it "is invalid when it is missing a title attribute" do
+    book = Book.new
+    book.valid?
+    expect(book.errors[:title].empty?).to be_false
+  end
+  
+  it "is valid when it has a pages attribute" do
+    book = Book.new pages: 250, title: "Something"
+    book.valid?
+    expect(book.errors[:pages].empty?).to be_true
+  end
+  
+  it "is invalid when it is missing a pages attribute" do
+    book = Book.new
+    book.valid?
+    expect(book.errors[:pages].empty?).to be_false
+  end
 end
