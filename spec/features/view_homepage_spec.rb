3bf9200dbc1a381dd5d679e26688ec399aa90e70
Tristan <tristan@rubadeau.com>
2014-04-14 19:15:01 +0000

    Green pass on book spec

--- spec/features/view_homepage_spec.rb
+++ spec/features/view_homepage_spec.rb
@@ -4,7 +4,7 @@ feature "View the homepage" do
   scenario " user sees correct information" do
     visit root_path
     
-    expect(page).to have_content "Welcome"
+    expect(page).to have_content "Welcome Home"
     expect(page).to have_title "Readathon App"
   end
 end
